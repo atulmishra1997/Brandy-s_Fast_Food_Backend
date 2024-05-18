40620fd6217cff6136b5200420d08084563961d6
Atul Mishra <147801384+atulmishra1997@users.noreply.github.com>
2024-05-19 00:43:48 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -48,7 +48,7 @@ const placeOrder = async (req,res) => {
             cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`
         })
 
-        res.json({success:true,session_url:session.url})
+        res.json({success:true,success_url:session.url})
 
     } catch (error) {
         console.log(error);
